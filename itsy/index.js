97148daec111527160c89898c2c1001715e14bce
Henry Catalini Smith <henry@catalinismith.com>
2019-02-10 21:07:23 +0100

    make it serve

--- itsy/index.js
+++ itsy/index.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-const http = require("http")
-const host = "127.0.0.1"
+const express = require("express")
+const serveStatic = require("serve-static")
+
+const app = express()
 const port = process.env.PORT || "8080"
 
-const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end("Hello World\n")
-})
+app.use(serveStatic(__dirname))
 
-server.listen(port, host, () => {
-  console.log(`itsy8 up and running on http://${host}:${port}/!`)
+app.listen(port, () => {
+  console.log(`itsy8 up and running on http://127.0.0.1:${port}/!`)
 })
 
