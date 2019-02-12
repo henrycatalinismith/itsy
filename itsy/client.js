a07abc3be3c6c99b7b1d1862b908b942dac426b0
Henry Catalini Smith <henry@catalinismith.com>
2019-02-12 21:21:57 +0100

    DOMContentLoaded

--- itsy/client.js
+++ itsy/client.js
@@ -1,27 +1,28 @@
-const body = document.body
-const canvas = document.createElement("canvas")
-const json = document.querySelector("script[type='application/json']").innerText
-const lua = document.querySelector("script[type='text/lua']").innerText
-const script = document.createElement("script")
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body
+  const canvas = document.createElement("canvas")
+  const json = document.querySelector("script[type='application/json']").innerText
+  const lua = document.querySelector("script[type='text/lua']").innerText
+  const script = document.createElement("script")
 
-const config = JSON.parse(json)
+  const config = JSON.parse(json)
 
-script.src = `/itsy.js?${config.version}`
-script.onload = () => {
-  Module({
-    arguments: [lua],
-    canvas,
-    onRuntimeInitialized: () => {
-      console.log('eee')
-    }
-  }).then(wasm => {
-    console.log(wasm)
-    //setTimeout(() => wasm.abort(), 1000)
-    //setTimeout(() => wasm.pauseMainLoop(), 1000)
-    //setTimeout(() => wasm.resumeMainLoop(), 2000)
-  })
-}
+  script.src = `/itsy.js?${config.version}`
+  script.onload = () => {
+    Module({
+      arguments: [lua],
+      canvas,
+      onRuntimeInitialized: () => {
+        console.log('eee')
+      }
+    }).then(wasm => {
+      console.log(wasm)
+      //setTimeout(() => wasm.abort(), 1000)
+      //setTimeout(() => wasm.pauseMainLoop(), 1000)
+      //setTimeout(() => wasm.resumeMainLoop(), 2000)
+    })
+  }
 
-body.appendChild(canvas)
-body.appendChild(script)
-
+  body.appendChild(canvas)
+  body.appendChild(script)
+})
