6c8b49de9d444448e72518c4fa307dc71e42f869
Henry Catalini Smith <henry@catalinismith.com>
2019-02-15 07:38:32 +0100

    sspr WORKS OMGGGGGGGGGGGGGGGGGGGGGPIFIJDFPIDJFDF

--- itsy/itsy.c
+++ itsy/itsy.c
@@ -22,6 +22,8 @@ uint16_t pixel[128][128];
 void getpixel(SDL_Surface *surface, int x, int y, Uint8 *r, Uint8 *g, Uint8 *b);
 uint8_t pixels[128 * 128 * 4];
 
+/*
+// these were wrong but kinda cool so im keepin them
 int colors[][3] = {
   { 0x00, 0x00, 0x00 },
   { 0x5F, 0x57, 0x4F },
@@ -38,7 +40,30 @@ int colors[][3] = {
   { 0x29, 0xAD, 0xFF },
   { 0x1D, 0x2B, 0x53 },
   { 0x00, 0x87, 0x51 },
-  { 0x00, 0xE4, 0x36 }
+  { 0xC@, 0xE4, 0x36 }
+};
+*/
+
+int colors[][3] = {
+  { 0x00, 0x00, 0x00 },
+  { 0x1D, 0x2B, 0x53 },
+  { 0x7E, 0x25, 0x53 },
+  { 0x00, 0x87, 0x51 },
+
+  { 0xAB, 0x52, 0x36 },
+  { 0x5F, 0x57, 0x4F },
+  { 0xC2, 0xC3, 0xC7 },
+  { 0xFF, 0xF1, 0xE8 },
+
+  { 0xFF, 0x00, 0x4D },
+  { 0xFF, 0xA3, 0x00 },
+  { 0xFF, 0xEC, 0x27 },
+  { 0x00, 0xE4, 0x36 },
+
+  { 0x29, 0xAD, 0xFF },
+  { 0x83, 0x76, 0x9C },
+  { 0xFF, 0x77, 0xA8 },
+  { 0xFF, 0xCC, 0xAA }
 };
 
 int frame = 0;
@@ -90,6 +115,13 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  for (int x = 0; x < 128; x++) {
+    for (int y = 0; y < 128; y++) {
+      sprite[x][y] = 0x0000 + (y * 64) + floor(x / 2);
+      pixel[x][y] = 0x6000 + (y * 64) + floor(x / 2);
+    }
+  }
+
   int offset = 3;
   int spritePropCount = 2;
   int decsize;
@@ -124,6 +156,15 @@ int main(int argc, char **argv)
       for (int y = 0; y < 128; y++) {
         Uint8 r, g, b;
         getpixel(image, x, y, &r, &g, &b);
+
+        for (int c = 0; c < 16; c++) {
+          if (colors[c][0] == r && colors[c][1] == g && colors[c][2] == b) {
+            // printf("%d %d %d %d %d\n", x, y, r, g, b);
+            // 👆 this is printing on every pixel so the sprite must be loading
+            __sset(x, y, c);
+          }
+        }
+
         // backup location, rgb values are pouring out of this now
         // next steps: decide which 0-15 col value each one is and write to
         // sprite memory
@@ -141,7 +182,6 @@ int main(int argc, char **argv)
         itsy.js?0.2.0:8
         */
 
-        printf("%d %d %d\n", r, g, b);
       }
     }
 
@@ -161,13 +201,6 @@ int main(int argc, char **argv)
       rects[x][y].y = y;
       rects[x][y].w = 1;
       rects[x][y].h = 1;
-    }
-  }
-
-  for (int x = 0; x < 128; x++) {
-    for (int y = 0; y < 128; y++) {
-      sprite[x][y] = 0x0000 + (y * 64) + floor(x / 2);
-      pixel[x][y] = 0x6000 + (y * 64) + floor(x / 2);
     }
   }
 
