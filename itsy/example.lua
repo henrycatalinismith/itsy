a19f713fda8fdcab94afc2e7597c0143ce882e57
Henry Catalini Smith <henry@catalinismith.com>
2019-02-17 16:45:59 +0100

    some pixel font work

--- itsy/example.lua
+++ itsy/example.lua
@@ -1,12 +1,16 @@
-print('lua!')
+--print('lua!')
 frame = 0
 
-function _updates()
+function _init()
+end
+
+
+function _update()
   frame = frame + 1
 
   cls()
 
-  camera(flr(frame/2), flr(frame/2))
+  --camera(flr(frame/2), flr(frame/2))
 
   sspr(32, 32, 32, 32, 32, 32, 32, 32)
 
@@ -26,40 +30,42 @@ function _updates()
   line(10, 110, 10, 10, 11)
   line(110, 10, 10, 110, 12)
 
+  print("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 0, 9)
+  print("abcdefghijklmnopqrstuvwxyz", 0, 6, 9)
 end
 
 cls(7)
 sspr(0, 0, 128, 128, 0, 0, 128, 128)
 
-print(2 + 2)
-print(abs(2 - 30))
-print(max(1, 2))
-print(min(3, 4))
-print(pow(3, 3))
-print(flr(10.99999999))
-print(ceil(99.1))
-print(3 + 2)
+--print(2 + 2)
+--print(abs(2 - 30))
+--print(max(1, 2))
+--print(min(3, 4))
+--print(pow(3, 3))
+--print(flr(10.99999999))
+--print(ceil(99.1))
+--print(3 + 2)
 
-print('---- random ----')
-print(rnd(10))
-print(rnd(10))
-print(rnd(10))
-print(rnd(10))
-print(rnd(10))
-print('---- /random ----')
+--print('---- random ----')
+--print(rnd(10))
+--print(rnd(10))
+--print(rnd(10))
+--print(rnd(10))
+--print(rnd(10))
+--print('---- /random ----')
 
 local cave = {}
-print(#cave)
+--print(#cave)
 add(cave, {2, 20})
-print(#cave)
-print(cave[1][1])
-print(cave[1][2])
+--print(#cave)
+--print(cave[1][1])
+--print(cave[1][2])
 del(cave, 1)
-print(#cave)
+--print(#cave)
 
-print(tonumber("10"))
-print(tostring({ 1, 2 }))
-print(type(10))
+--print(tonumber("10"))
+--print(tostring({ 1, 2 }))
+--print(type(10))
 
 circ(64, 64, 32, 8)
 
@@ -69,7 +75,7 @@ dots = {
 }
 
 for k, v in pairs(dots) do
-  print(k)
+  --print(k)
   circ(v[1], v[2], 10, 7)
 end
 
