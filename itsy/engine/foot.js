44aaf6bee80432fbfb632eabd20c3847daf31250
Henry Catalini Smith <henry@catalinismith.com>
2019-03-17 16:14:36 +0100

    try this way

--- itsy/engine/foot.js
+++
@@ -1,4 +0,0 @@
-Module({
-  arguments: argv,
-  canvas,
-})
---
+++ itsy/engine/template.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const canvas = document.querySelector("canvas")
+
+  const lua = document
+    .querySelector("script[type='text/lua']")
+    .innerText
+
+  const palette = document
+    .querySelector("img[width='8'][height='8']")
+    .src.split(",")[1]
+
+  const spritesheet = document
+    .querySelector("img[width='128'][height='128']")
+    .src.split(",")[1]
+
+  const argv = [lua, palette, spritesheet]
+
+  var Module= {
+    arguments: argv,
+    canvas,
+  }
+
+  // itsy.c
+  //--:Module:--//
+
+})
