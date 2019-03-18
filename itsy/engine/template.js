7465a54bba72d6227db5cd265672289d7a232158
Henry Catalini Smith <henry@catalinismith.com>
2019-03-18 20:08:46 +0100

    ditch the pageload event

--- itsy/engine/template.js
+++ itsy/engine/template.js
@@ -1,31 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas")
 
-  const lua = document.querySelector("script[type='text/lua']").innerText
+const lua = document.querySelector("script[type='text/lua']").innerText
 
-  const palette = document
-    .querySelector("img[width='8'][height='8']")
-    .src.split(",")[1]
+const palette = document
+  .querySelector("img[width='8'][height='8']")
+  .src.split(",")[1]
 
-  const spritesheet = document
-    .querySelector("img[width='128'][height='128']")
-    .src.split(",")[1]
+const spritesheet = document
+  .querySelector("img[width='128'][height='128']")
+  .src.split(",")[1]
 
-  const argv = [
-    lua,
-    palette,
-    spritesheet,
-    `${canvas.offsetWidth}`,
-    `${canvas.offsetHeight}`,
-  ]
+const argv = [
+  lua,
+  palette,
+  spritesheet,
+  `${canvas.offsetWidth}`,
+  `${canvas.offsetHeight}`,
+]
 
-  console.log(argv)
+console.log(argv)
 
-  var Module = {
-    arguments: argv,
-    canvas
-  }
+var Module = {
+  arguments: argv,
+  canvas
+}
 
-  // itsy.c
-  //--:Module:--//
-});
+// itsy.c
+//--:Module:--//
