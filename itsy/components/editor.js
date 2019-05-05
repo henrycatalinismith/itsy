bb86b6c116722ce730f0cddc5d573b112b80b995
Henry Catalini Smith <henry@catalinismith.com>
2019-05-05 17:59:48 +0200

    add editor

---
+++ itsy/components/editor.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { connect } from "react-redux"
+import { Controlled as CodeMirror } from "react-codemirror2"
+import styles from "../stylesheets/editor.module.scss"
+import actions from "../editor/actions"
+
+export default connect(
+  ({ loading, lua }) => ({
+    loading,
+    lua,
+  }), actions
+)(({
+  change,
+  loading,
+  lua,
+}) => {
+
+  const options = {
+    autocapitalize: false,
+    autocorrect: false,
+    cursorBlinkRate: 500,
+    indentUnit: 1,
+    indentWithTabs: false,
+    lineNumbers: true,
+    mode: "lua",
+    spellcheck: false,
+    tabSize: 1,
+    theme: "itsy",
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div className={styles.loading}>
+          loading
+        </div>
+      ) : (
+        <CodeMirror
+          className={styles.editor}
+          value={lua}
+          options={options}
+          onBeforeChange={(...[,, value]) => change(value)}
+        />
+      )}
+    </>
+  )
+})
---
+++ itsy/editor/actions.js
@@ -0,0 +1,8 @@
+import { action } from "@highvalley.systems/signalbox"
+
+export default {
+  ...action("change", ["lua"]),
+  ...action("impression", []),
+  ...action("inject", ["lua"]),
+  ...action("ready", ["loading"]),
+}
