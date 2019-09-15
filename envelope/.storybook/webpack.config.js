d7bf7d36cfe4d02089df5d604dd6be46122463bb
Henry Catalini Smith <henry@catalinismith.com>
2019-09-15 15:32:52 +0200

    layout

--- envelope/.storybook/webpack.config.js
+++ envelope/.storybook/webpack.config.js
@@ -20,8 +20,6 @@ module.exports = ({ config, mode }) => {
         options: {
           importLoaders: 2,
           modules: true,
-          // localIdentName: "[name]__[local]___[hash:base64:5]",
-          // localIdentName: "[local]",
         },
       },
       {
