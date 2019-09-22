e81f46e2c2160da2b28fd289686448ef46935330
Henry Catalini Smith <henry@catalinismith.com>
2019-09-22 23:08:40 +0200

    ident

--- envelope/ident/ident.js
+++ envelope/ident/ident.js
@@ -4,9 +4,21 @@ import Wordmark from "@highvalley.systems/logotype/wordmark"
 import styles from "./ident.module.scss"
 
 export default function Ident() {
+  const [height, setHeight] = React.useState(undefined)
+
+  React.useEffect(() => {
+    if (typeof window === "undefined") {
+      return
+    }
+    const ratio = window.innerWidth / 256
+    setHeight(window.innerHeight / ratio)
+  }, [])
+
   return (
-    <Svg width={512} height={512} className={styles.svg}>
-      <Wordmark />
+    <Svg width={256} height={height || 256} className={styles.svg}>
+      {height && (
+        <Wordmark x={49} y={(height / 2) - 24} animation="splash" />
+      )}
     </Svg>
   )
 }
