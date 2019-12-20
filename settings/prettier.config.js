07348fdc8c55832ad3bbb188842e25265548ada0
Henry Catalini Smith <henry@catalinismith.com>
2019-12-21 00:37:14 +0100

    unify prettier.config.js

---
+++ settings/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+}
