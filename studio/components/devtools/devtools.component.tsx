595bc6261d3e77f5963a8831eedd5b3a530b4431
Henry Catalini Smith <henry@catalinismith.com>
2019-12-23 00:23:44 +0100

    devtools height dance starting to work now

--- studio/components/devtools/devtools.component.tsx
+++ studio/components/devtools/devtools.component.tsx
@@ -5,25 +5,38 @@ import {
   DevtoolsState,
   selectDevtools,
 } from "@itsy.studio/studio/store/devtools"
+import {
+  KeyboardState,
+  selectKeyboard,
+} from "@itsy.studio/studio/store/keyboard"
+import { ScreenState, selectScreen } from "@itsy.studio/studio/store/screen"
 import DevtoolsPlayPanel from "@itsy.studio/studio/components/devtools-play-panel"
 import DevtoolsToolbar from "@itsy.studio/studio/components/devtools-toolbar"
 import styles from "@itsy.studio/studio/components/devtools/devtools.module.scss"
 
 interface DevtoolsProps {
   devtools: DevtoolsState
+  keyboard: KeyboardState
+  screen: ScreenState
 }
 
 const mapStateToProps = (state) => ({
   devtools: selectDevtools(state),
+  keyboard: selectKeyboard(state),
+  screen: selectScreen(state),
 })
 
 const mapDispatchToProps = {}
 
-export function Devtools({ devtools }: DevtoolsProps) {
+export function Devtools({ devtools, keyboard, screen }: DevtoolsProps) {
+  let height = 26
+  if (!keyboard.visible) {
+    height += screen.width
+  }
   return (
-    <View style={styles.devtools}>
+    <View style={{ ...styles.devtools, height }}>
       <DevtoolsToolbar />
-      <DevtoolsPlayPanel />
+      {!keyboard.visible && <DevtoolsPlayPanel />}
     </View>
   )
 }
