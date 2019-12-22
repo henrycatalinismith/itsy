d5f92bd63db80588feceb0882f9d002ddfdcc327
Henry Catalini Smith <henry@catalinismith.com>
2019-12-22 21:17:01 +0100

    dependency cleanup for studio

--- studio/middlewares/log.ts
+++
@@ -1,7 +0,0 @@
-import { before, after } from "@highvalley.systems/signalbox"
-
-export default [
-  after(/.*/, (store, action) => {
-    console.log([`[${new Date().toISOString()}]`, `${action.type}`].join(" "))
-  }),
-]
