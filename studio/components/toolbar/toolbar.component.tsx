ad913158bd91ada0396827f5d5d7b3fcf5c72051
Henry Catalini Smith <henry@catalinismith.com>
2020-01-19 19:46:10 +0100

    fix toolbar styling for tiles mode

--- studio/components/toolbar/toolbar.component.tsx
+++ studio/components/toolbar/toolbar.component.tsx
@@ -2,29 +2,50 @@ import React from "react"
 import { View } from "react-native"
 import { connect } from "react-redux"
 
-import { Panel, selectRankedPanels } from "@itsy.studio/studio/store/panels"
+import {
+  PanelMode,
+  Panel,
+  selectPanelMode,
+  selectRankedPanels,
+} from "@itsy.studio/studio/store/panels"
 
 import Tab from "@itsy.studio/studio/components/tab"
 import styles from "./toolbar.module.scss"
 
 interface ToolbarProps {
+  panelMode: PanelMode
   panels: Panel[]
 }
 
 const mapStateToProps = (state) => ({
+  panelMode: selectPanelMode(state),
   panels: selectRankedPanels(state),
 })
 
 const mapDispatchToProps = {}
 
-export function Toolbar({ panels }: ToolbarProps) {
-  return (
-    <View style={styles.toolbar}>
-      {panels.map((panel) => (
-        <Tab key={panel.id} id={panel.id} />
-      ))}
-    </View>
-  )
+export function Toolbar({ panelMode, panels }: ToolbarProps) {
+  switch (panelMode) {
+    case PanelMode.slide:
+      return (
+        <View style={[styles.toolbar, styles.slide]}>
+          {panels.map((panel) => (
+            <Tab key={panel.id} id={panel.id} />
+          ))}
+        </View>
+      )
+
+    case PanelMode.tiles:
+      return (
+        <View style={[styles.toolbar, styles.tiles]}>
+          <View style={[styles.corner]}>
+            {panels.map((panel) => (
+              <Tab key={panel.id} id={panel.id} />
+            ))}
+          </View>
+        </View>
+      )
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Toolbar)
