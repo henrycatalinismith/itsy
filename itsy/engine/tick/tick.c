88273fe78a8ce87f71eb5b8df6bc18601107ec63
Henry Catalini Smith <henry@catalinismith.com>
2020-01-23 20:11:24 +0100

    fix touch position

--- itsy/engine/tick/tick.c
+++ itsy/engine/tick/tick.c
@@ -29,13 +29,23 @@ void tick (void)
 
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
-      //case SDL_FINGERDOWN:
-        //printf("finger %d, %d\n", (int)event.tfinger.x, (int)event.tfinger.y);
-        //break;
+      case SDL_FINGERDOWN:
+        printf("finger %f, %f\n", event.tfinger.x, event.tfinger.y);
+        poke(TOUCH_0_B, true);
+        poke(TOUCH_0_X, (int) floor(event.tfinger.x * 128));
+        poke(TOUCH_0_Y, (int) floor(event.tfinger.y * 128));
+        break;
 
-      //case SDL_FINGERUP:
-        //printf("SDL_FINGERUP\n");
-        //break;
+      case SDL_FINGERUP:
+        poke(TOUCH_0_B, false);
+        printf("SDL_FINGERUP %f, %f\n", event.tfinger.x, event.tfinger.y);
+        break;
+
+      case SDL_FINGERMOTION:
+        printf("SDL_FINGERMOTION %d, %d\n", (int) floor(event.tfinger.x * 128), (int) floor(event.tfinger.y * 128));
+        poke(TOUCH_0_X, (int) floor(event.tfinger.x * 128));
+        poke(TOUCH_0_Y, (int) floor(event.tfinger.y * 128));
+        break;
 
       case SDL_MOUSEBUTTONDOWN:
         printf("mouse %d, %d\n", event.button.x, event.button.y);
@@ -59,10 +69,6 @@ void tick (void)
           poke(TOUCH_0_Y, 0);
         }
         break;
-
-      //case SDL_FINGERMOTION:
-        //printf("SDL_FINGERMOTION\n");
-        //break;
 
       case SDL_MOUSEMOTION:
         if (peek(TOUCH_0_B)) {
