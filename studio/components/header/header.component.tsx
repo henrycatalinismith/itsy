0e67470b1cd241537029e9163c574a2930d3d25e
Henry Catalini Smith <henry@catalinismith.com>
2020-01-26 12:20:15 +0100

    show active disk in header

--- studio/components/header/header.component.tsx
+++ studio/components/header/header.component.tsx
@@ -2,26 +2,44 @@ import React from "react"
 import { View } from "react-native"
 import { connect } from "react-redux"
 
+import DiskIcon from "@itsy.studio/studio/components/disk-icon"
 import Floppy from "@itsy.studio/studio/components/floppy"
 import Font from "@itsy.studio/studio/components/font"
 import Storage from "@itsy.studio/studio/components/storage"
+import {
+  Disk,
+  DiskType,
+  selectActiveDisk,
+} from "@itsy.studio/studio/store/disks"
 
 import styles from "./header.module.scss"
 
-interface HeaderProps {}
+interface HeaderProps {
+  disk: Disk
+}
 
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => ({
+  disk: selectActiveDisk(state),
+})
 
 const mapDispatchToProps = {}
 
-export function Header({}: HeaderProps) {
+export function Header({ disk }: HeaderProps) {
   return (
     <View style={styles.header}>
       <View style={styles.icon}>
-        <Floppy />
+        {disk.type === DiskType.empty ? (
+          <Floppy />
+        ) : (
+          <DiskIcon id={disk.id} size={24} />
+        )}
       </View>
       <View style={styles.title}>
-        <Font>itsy.studio</Font>
+        {disk.type === DiskType.empty ? (
+          <Font>itsy.studio</Font>
+        ) : (
+          <Font>{disk.name}</Font>
+        )}
       </View>
       <View style={styles.storage}>
         <Storage />
