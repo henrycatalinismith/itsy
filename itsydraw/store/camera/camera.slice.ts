f67be6625397c7b178a6f29b26301daace5dcb19
Henry Catalini Smith <henry@catalinismith.com>
2020-03-11 22:17:59 +0100

    bit of quick camera snap math (broken slightly)

--- itsydraw/store/camera/camera.slice.ts
+++ itsydraw/store/camera/camera.slice.ts
@@ -9,7 +9,7 @@ import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit"
 const name = "camera"
 
 const initialState: Rect = {
-  x: 16,
+  x: 0,
   y: 0,
   width: 64,
   height: 64,
@@ -30,6 +30,17 @@ const reducers = {
     if (camera.y < 0) {
       camera.y = 0
     }
+
+    if (camera.x + camera.width > 127) {
+      camera.x = 127 - camera.width
+    }
+
+    if (camera.y + camera.height > 127) {
+      camera.y = 127 - camera.height
+    }
+
+    camera.x = Math.ceil(camera.x / camera.width) * camera.width
+    camera.y = Math.ceil(camera.y / camera.height) * camera.height
   },
 
   zoom(camera, action: PayloadAction<number>): void {
@@ -48,8 +59,16 @@ export const moveCamera = (x: number, y: number): Thunk => async (
   dispatch,
   getState
 ) => {
-  const zoom = selectZoom(getState())
-  const point = { x, y }
+  const camera = selectCamera(getState())
+  const point = {
+    x: Math.round(x / camera.width) * (camera.width - 1),
+    y: Math.round(y / camera.height) * (camera.height - 1),
+  }
+
+  if (point.x === camera.x || point.y === camera.y) {
+    console.log("no", x, y, point.x, point.y)
+    return
+  }
 
   dispatch(slice.actions.move(point))
 }
