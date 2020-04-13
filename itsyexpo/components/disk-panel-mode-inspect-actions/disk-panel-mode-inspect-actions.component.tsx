b278c724cab74b9d36019d2bcdf45412b0aa8d0c
Henry Catalini Smith <henry@catalinismith.com>
2020-04-13 13:42:31 +0200

    tidy

--- itsyexpo/components/disk-panel-mode-inspect-actions/disk-panel-mode-inspect-actions.component.tsx
+++ itsyexpo/components/disk-panel-mode-inspect-actions/disk-panel-mode-inspect-actions.component.tsx
@@ -1,35 +1,26 @@
-import Button, {
-  ButtonThemes,
-} from "@highvalley.systems/itsyexpo/components/button"
+import { ButtonThemes } from "@highvalley.systems/itsyexpo/components/button"
 import DiskPanelModeInspectButtonGroup, {
   DiskPanelModeInspectButtonGroupProps,
 } from "@highvalley.systems/itsyexpo/components/disk-panel-mode-inspect-button-group"
-import Font from "@highvalley.systems/itsyexpo/components/font"
 import {
   DiskPanelModes,
   setDiskPanelMode,
 } from "@highvalley.systems/itsyexpo/store/disk-panel"
-import { shareDisk } from "@highvalley.systems/itsyexpo/store/disks"
 import React from "react"
-import { Text, View } from "react-native"
 import { connect } from "react-redux"
-import styles from "./disk-panel-mode-inspect-actions.module.scss"
 
 interface DiskPanelModeInspectActionsProps {
   setDiskPanelMode: (mode: DiskPanelModes) => void
-  shareDisk: () => void
 }
 
 const mapStateToProps = (state, { id }) => ({})
 
 const mapDispatchToProps = {
   setDiskPanelMode,
-  shareDisk,
 }
 
 export function DiskPanelModeInspectActions({
   setDiskPanelMode,
-  shareDisk,
 }: DiskPanelModeInspectActionsProps) {
   const title = "actions"
   const buttons = []
