d8d12a26ef11b3b0df0814433f1c3eb828340285
Henry Catalini Smith <henry@catalinismith.com>
2020-07-15 19:06:45 +0200

    nav

--- itsyexpo/screens/disk/disk.screen.tsx
+++ itsyexpo/screens/disk/disk.screen.tsx
@@ -1,7 +1,9 @@
+import colors from "@highvalley.systems/palettes/pico8/original.es6"
 import { RootStackParamList } from "@highvalley.systems/itsyexpo/screens"
 import { RouteProp } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import Font from "@highvalley.systems/itsyexpo/components/font"
 import Code from "@highvalley.systems/itsyexpo/screens/code/code.screen"
 import Draw from "@highvalley.systems/itsyexpo/screens/draw/draw.screen"
 import Meta from "@highvalley.systems/itsyexpo/screens/meta/meta.screen"
@@ -9,6 +11,12 @@ import Play from "@highvalley.systems/itsyexpo/screens/play"
 import SafeArea from "@highvalley.systems/itsyexpo/components/safe-area"
 import React from "react"
 import { connect } from "react-redux"
+import { View } from "react-native"
+import {
+  MaterialTopTabNavigationConfig,
+  MaterialTopTabBarOptions,
+} from "@react-navigation/material-top-tabs/lib/typescript/src/types"
+import styles from "./disk.module.scss"
 
 export interface DiskScreenProps {
   navigation: StackNavigationProp<RootStackParamList, "Disk">
@@ -28,9 +36,29 @@ const mapDispatchToProps = {}
 const Tab = createMaterialTopTabNavigator()
 
 export function DiskScreen({ navigation, route }: DiskScreenProps) {
+  const tabBarOptions: MaterialTopTabBarOptions = {
+    showLabel: false,
+    labelStyle: {
+      color: "#ff0000",
+    },
+    renderBadge: (props: any) => {
+      return (
+        <View style={{ position: "absolute", right: 28, top: 5 }}>
+          <Font fontSize={18}>{props.route.name.toLowerCase()}</Font>
+        </View>
+      )
+    },
+    style: styles.tabBar,
+    indicatorStyle: styles.indicator,
+  }
+
+  const navigator: MaterialTopTabNavigationConfig = {
+    tabBarOptions,
+  }
+
   return (
     <SafeArea>
-      <Tab.Navigator>
+      <Tab.Navigator {...navigator}>
         <Tab.Screen
           name="Play"
           component={Play}
