d230552e33385893782580d9fab579cca6834f21
Henry Catalini Smith <henry@catalinismith.com>
2020-07-18 21:35:22 +0200

    fixyfixy

--- itsyexpo/components/back/back.component.tsx
+++ itsyexpo/components/back/back.component.tsx
@@ -1,18 +1,37 @@
+import { PlayerModes } from "@highvalley.systems/itsyexpo/store/player"
+import { RootState } from "@highvalley.systems/itsyexpo/store"
 import Font from "@highvalley.systems/itsyexpo/components/font"
-import { TouchableOpacity } from "react-native"
+import { ActivityIndicator, TouchableOpacity, View } from "react-native"
 import React from "react"
+import { connect } from "react-redux"
+import colors from "@highvalley.systems/palettes/pico8/original.es6"
 import styles from "./back.module.scss"
 
 export interface BackProps {
   onPress: () => void
+  playerMode: PlayerModes
 }
 
-export function Back({ onPress }: BackProps): React.ReactElement {
+const mapStateToProps = (state: RootState) => ({
+  playerMode: state.player.mode,
+})
+
+const mapDispatchToProps = {}
+
+export function Back({ playerMode, onPress }: BackProps): React.ReactElement {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.component}>
-      <Font fontSize={24}>{"<"}</Font>
-    </TouchableOpacity>
+    <View style={styles.component}>
+      {[PlayerModes.Idle].includes(playerMode) && (
+        <TouchableOpacity onPress={onPress}>
+          <Font fontSize={24}>{"<"}</Font>
+        </TouchableOpacity>
+      )}
+
+      {[PlayerModes.Busy, PlayerModes.Halt, PlayerModes.Load].includes(
+        playerMode
+      ) && <ActivityIndicator color={colors[0]} />}
+    </View>
   )
 }
 
-export default Back
+export default connect(mapStateToProps, mapDispatchToProps)(Back)
